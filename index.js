e2df5ace6a187c087d4262b6865047c6b68e3e5b
tujunjia <273288259@qq.com>
2019-12-12 14:20:50 +0800

    项目的初始化

---
+++ index.js
@@ -0,0 +1,85 @@
+//引入http核心模块
+var http = require('http')
+//引入fs核心模块
+var fs = require('fs')
+//引入art-template核心模块
+var aTemplate = require("art-template");
+
+
+
+
+//创建一个http服务
+var server = http.createServer()
+
+server.on('request',function(req, res){
+  
+  res.setHeader('Content-Type', 'text/html');  //设置数据返回的格式
+  var url = req.url
+  console.log(url)
+  if (url === '/') {
+    fs.readFile('./view/index.html',function(error,data){
+      if (error) {
+        console.log('index.html 文件读取错误')
+        return res.end ('404 nont found.')
+      }
+      res.setHeader('Content-Length',Buffer.byteLength(data))  //设置返回数据的长度 
+      res.end(data)
+
+    })
+  } else if ( url.indexOf('/public/') === 0) {
+    fs.readFile('.' + url, function(error, data){
+      if (error) {
+        console.log('public静态读取错误')
+        return res.end ('404 nont found.')
+      }
+      res.setHeader('Content-Type', 'text/css');  //设置数据返回的格式
+      return res.end (data)
+    })
+  } else if (url === '/www') {
+    
+      fs.readFile('./view/index.html', function (error, data) {
+        if (error) {
+          return res.end('模板文件index.html读取错误')
+        }
+        var data_html = data.toString()
+        fs.readdir('./www/', function (error, data) {
+          if (error) {
+            console.log('www目录读取错误')
+            return res.end('www目录读取错误')
+          }
+        console.log(data)
+        var htmlStr = aTemplate.render(data_html,{
+          data : data,
+          title : '服务端渲染页面'
+        })
+        console.log(htmlStr)
+        res.end(htmlStr)
+      })
+    })
+  } else if (url === '/item/') {
+    console.log('来到item页面')
+    fs.readFile('./view/item.html', function(error, data){
+    if (error) {
+      return res.end ('404 cosole')
+    }
+    res.end (data)
+   })
+  } else {  
+    console.log('来到404页面')
+    fs.readFile('./view/404.html', function(error, data){
+    if (error) {
+      return res.end ('404 cosole')
+    }
+    res.end (data)
+   })
+  }
+})
+
+// 监听端口
+server.listen('3000',function(error, data){
+  if (error) {
+    console.log('node connet error')
+  } else {
+    console.log('server is runing ...')
+  }
+})
---
+++ public/js/index.js
@@ -0,0 +1,2 @@
+
+  // window.alert("js加载成功")
